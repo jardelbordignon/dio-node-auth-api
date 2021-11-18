31edee62ac40408f7faba77ca4e27fe6d9745ece
Jardel <jardel1101@gmail.com>
2021-11-18 19:19:45 -0300

    Add express

--- LIVE-01/src/index.ts
+++ LIVE-01/src/index.ts
@@ -1,1 +1,9 @@
-console.log('Start')
+import express, { Request, Response, NextFunction } from 'express'
+
+const app = express()
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ message: 'Ok!'})
+})
+
+app.listen(3000, () => console.log('App running in port 3000'))
