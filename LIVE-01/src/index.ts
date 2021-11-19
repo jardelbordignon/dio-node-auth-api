6f68cdba3c8032ca29f8a242f3e1362cd93cf681
Jardel <jardel1101@gmail.com>
2021-11-18 21:04:10 -0300

    User post

--- LIVE-01/src/index.ts
+++ LIVE-01/src/index.ts
@@ -1,8 +1,11 @@
-import express, { Request, Response, NextFunction } from 'express'
+import express, { Request, Response, NextFunction, urlencoded } from 'express'
 
 import { userRoutes } from './routes'
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/users', userRoutes)
 
--- LIVE-01/src/routes/user.routes.ts
+++ LIVE-01/src/routes/user.routes.ts
@@ -1,21 +1,45 @@
 import { Router, Request, Response, NextFunction } from 'express'
 import { StatusCodes } from 'http-status-codes'
+import { v4 as uuid } from 'uuid'
 
-const { OK } = StatusCodes
+const { OK, CREATED } = StatusCodes
 const userRoutes = Router()
 
-const users = [{}]
+interface IUser {
+  id?: string
+  name: string
+  email: string
+  password: string
+}
 
-// /users
+const users: IUser[] = []
+
+// get /users
 userRoutes.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.status(OK).json({users})
+  res.status(OK).json(users)
 })
 
-// /users/:uuid
+// get /users/:uuid
 userRoutes.get('/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
   const { uuid } = req.params
 
-  res.status(OK).send({uuid})
+  const user = users.find(user => user.id === uuid)
+
+  if (!user) {
+    throw new Error('User not found')
+  }
+
+  res.status(OK).send(user)
+})
+
+// post /users
+userRoutes.post('/', (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body as IUser
+  const user = { id: uuid(), name, email, password }
+
+  users.push(user)
+
+  res.status(CREATED).send(user)
 })
 
 export { userRoutes }
