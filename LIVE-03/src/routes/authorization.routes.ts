983ed29b33fb15dc4823f88952d4eecf5f2774c6
Jardel <jardel1101@gmail.com>
2021-11-22 05:31:37 -0300

    Generate JWT token

--- LIVE-03/src/routes/authorization.routes.ts
+++ LIVE-03/src/routes/authorization.routes.ts
@@ -1,7 +1,11 @@
 import { Router, Request, Response, NextFunction } from 'express'
+import { sign } from 'jsonwebtoken'
+import { StatusCodes } from 'http-status-codes'
 
 import { ForbiddenError } from '@/models/errors'
 import { userRepository } from '@/repositories/user.repository'
+
+const { OK } = StatusCodes
 
 const authorizationRoutes = Router()
 
@@ -29,8 +33,26 @@ authorizationRoutes.post('/token', async (req: Request, res: Response, next: Nex
 
     const user = await userRepository.findByCredentials(username, password)
 
-    console.log(user)
-    
+    if (!user)
+      throw new ForbiddenError('Invalid credentials')
+
+    /* informações disponívies no jwt
+    iss - O domínio da aplicação geradora do token
+    sub - O assunto do token, mas é muito utilizado para guardar o ID do usuário
+    aud - Define a audição, quem pode utilizar esse token
+    exp - Data de expiração
+    nbf - Data em que o token pode ser aceito, um delay para que ele entre em vigor
+    iat - Data da criação do token
+    jti - Id do token
+    */
+
+    const payload = { username: user.username }
+    const options = { subject: user.uuid }
+    const secretKey = 'xxxxxxxxxx'
+
+    const jwt = sign(payload, secretKey, options)
+
+    res.status(OK).json({ jwt })
 
   } catch (error) {
     next(error)
